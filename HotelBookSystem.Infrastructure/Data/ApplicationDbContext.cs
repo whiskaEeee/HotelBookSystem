2159a2744f7a020ac00f3d0f1bcea168de4c4b78
Toshiro Kusahito <grishakovdima456@gmail.com>
2025-04-21 10:54:04 +0300

    Add Migration for Hotels table

--- HotelBookSystem.Infrastructure/Data/ApplicationDbContext.cs
+++ HotelBookSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HotelBookSystem.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelBookSystem.Infrastructure.Data
@@ -14,5 +15,6 @@ namespace HotelBookSystem.Infrastructure.Data
             
         }
 
+        public DbSet<Hotel> Hotels { get; set; }
     }
 }
