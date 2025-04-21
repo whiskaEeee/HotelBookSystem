7a9924f0c02b8f3cbd308097461d98624070625f
Toshiro Kusahito <grishakovdima456@gmail.com>
2025-04-21 10:41:13 +0300

    Added Hotel Model

---
+++ HotelBookSystem.Domain/Entities/Hotel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace HotelBookSystem.Domain.Entities
+{
+    public class Hotel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(64)]
+        public required string Name { get; set; }
+        [MaxLength(256)]
+        public string? Description { get; set; }
+        [Display(Name = "Цена за ночь")]
+        public double? Price { get; set; }
+        [Range(1, 3)]
+        public int Occupancy { get; set; }
+        [NotMapped]
+        public IFormFile? Image { get; set; }
+        [Display(Name = "Image URL")]
+        public string? ImageUrl { get; set; }
+        public DateOnly Last_Update { get; set; }
+
+
+    }
+}
