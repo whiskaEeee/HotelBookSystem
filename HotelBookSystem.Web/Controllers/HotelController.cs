0a4a4e9a7726565ed8c6663e413ec7ebdc5346db
Toshiro Kusahito <grishakovdima456@gmail.com>
2025-04-24 09:49:42 +0300

    changes

---
+++ HotelBookSystem.Web/Controllers/HotelController.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using HotelBookSystem.Infrastructure.Data;
+using HotelBookSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBookSystem.Web.Controllers
+{
+    public class HotelController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public HotelController(ApplicationDbContext db  )
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var Hotels = _db.Hotels.ToList();
+            return View(Hotels);
+        }
+
+    }
+}
