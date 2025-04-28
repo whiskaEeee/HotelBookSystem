28fa632a79ee6bdf5079aa0c3f886deba08a3c29
PC_home <grishakovdima456@gmail.com>
2025-04-28 07:38:01 +0300

    create update method

--- HotelBookSystem.Web/Controllers/HotelController.cs
+++ HotelBookSystem.Web/Controllers/HotelController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using HotelBookSystem.Domain.Entities;
 using HotelBookSystem.Infrastructure.Data;
 using HotelBookSystem.Web.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -18,6 +19,33 @@ namespace HotelBookSystem.Web.Controllers
         {
             var Hotels = _db.Hotels.ToList();
             return View(Hotels);
+        }
+
+        [HttpGet]
+        public IActionResult Update(int Id)
+        {
+            Hotel? item = _db.Hotels.FirstOrDefault(h => h.Id == Id);
+            if (item is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(item);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Hotel obj)
+        {
+            if(obj is not null)
+            {
+                if (ModelState.IsValid && obj.Id > 0)
+                {
+                    _db.Hotels.Update(obj);
+
+                    _db.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
+
         }
 
     }
