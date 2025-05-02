6e7c9f04d293fcc512658e51c7432dd010362d73
Toshiro Kusahito <grishakovdima456@gmail.com>
2025-05-02 22:29:31 +0300

    feat: add Hotel Repository interface and realization

---
+++ HotelBookSystem.Application/Common/Interfaces/IHotelRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using HotelBookSystem.Domain.Entities;
+
+namespace HotelBookSystem.Application.Common.Interfaces
+{
+    public interface IHotelRepository : IRepository<Hotel>
+    {
+        void Update(Hotel entity);
+    }
+}
---
+++ HotelBookSystem.Infrastructure/Repository/HotelRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using HotelBookSystem.Application.Common.Interfaces;
+using HotelBookSystem.Domain.Entities;
+using HotelBookSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookSystem.Infrastructure.Repository
+{
+    public class HotelRepository : Repository<Hotel>, IHotelRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public HotelRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Hotel entity)
+        {
+            _db.Hotels.Update(entity);
+        }
+    }
+}
