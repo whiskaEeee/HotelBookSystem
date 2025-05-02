1fda6e9a01074f016a74a68bc69fd54a0e908483
Toshiro Kusahito <grishakovdima456@gmail.com>
2025-05-02 22:32:14 +0300

    feat: add Hotel Number entity model

---
+++ HotelBookSystem.Domain/Entities/HotelNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace HotelBookSystem.Domain.Entities
+{
+    public class HotelNumber
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Hotel_Number { get; set; }
+
+        [ForeignKey("Hotel")]
+        public int HotelId { get; set; }
+        [ValidateNever]
+        public Hotel Hotel { get; set; }
+        public string? Details { get; set; }
+    }
+}
