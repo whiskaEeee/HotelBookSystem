69b965ee3548d151ae847f3f9d769c30d6e44f1e
Toshiro Kusahito <grishakovdima456@gmail.com>
2025-05-02 22:23:30 +0300

    feat: add UnitOfWork pattern

---
+++ HotelBookSystem.Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBookSystem.Application.Common.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IHotelRepository Hotel { get; }
+        IHotelNumberRepository HotelNumber { get; }
+        void Save();
+    }
+}
---
+++ HotelBookSystem.Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelBookSystem.Application.Common.Interfaces;
+using HotelBookSystem.Infrastructure.Data;
+
+namespace HotelBookSystem.Infrastructure.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public IHotelRepository Hotel {  get; private set; }
+        public IHotelNumberRepository HotelNumber { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Hotel = new HotelRepository(_db);
+            HotelNumber = new HotelNumberRepository(_db);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
