fe055c684fbd907bfac87074e9ffca54ba5b230a
PC_home <grishakovdima456@gmail.com>
2025-05-05 07:40:37 +0300

    feat: add amenity controller

---
+++ HotelBookSystem.Web/Controllers/AmenityController.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using HotelBookSystem.Application.Common.Interfaces;
+using HotelBookSystem.Domain.Entities;
+using HotelBookSystem.Infrastructure.Data;
+using HotelBookSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace HotelBookSystem.Web.Controllers
+{
+    public class AmenityController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AmenityController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
