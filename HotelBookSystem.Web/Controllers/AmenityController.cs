593fe7ccb0549ec026355d10075c35d294b73b1a
Toshiro Kusahito <grishakovdima456@gmail.com>
2025-05-05 11:06:15 +0300

    feat: add CRUD functionality to AmenityController

--- HotelBookSystem.Web/Controllers/AmenityController.cs
+++ HotelBookSystem.Web/Controllers/AmenityController.cs
@@ -3,7 +3,9 @@ using HotelBookSystem.Application.Common.Interfaces;
 using HotelBookSystem.Domain.Entities;
 using HotelBookSystem.Infrastructure.Data;
 using HotelBookSystem.Web.Models;
+using HotelBookSystem.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace HotelBookSystem.Web.Controllers
@@ -20,8 +22,123 @@ namespace HotelBookSystem.Web.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var amenities = _unitOfWork.Amenity.GetAll(includeProperties: "Hotel");
+            return View(amenities);
         }
 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            AmenityVM amenityVM = new()
+            {
+                HotelList = _unitOfWork.Hotel.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                })
+            };
+            return View(amenityVM);
+        }
+
+        [HttpPost]
+        public IActionResult Create(AmenityVM amenityVM)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Amenity.Add(amenityVM.Amenity);
+                _unitOfWork.Save();
+                TempData["success"] = "Успешно добавлено";
+                return RedirectToAction(nameof(Index));
+            }
+
+            amenityVM.HotelList = _unitOfWork.Hotel.GetAll().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+
+            return View(amenityVM);
+        }
+
+        [HttpGet]
+        public IActionResult Update(int amenityId)
+        {
+            AmenityVM amenityVM = new()
+            {
+                HotelList = _unitOfWork.Hotel.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+
+                Amenity = _unitOfWork.Amenity.Get(u => u.Id == amenityId)
+            };
+
+            if (amenityVM.Amenity is null)
+            {
+                RedirectToAction("Error", "Home");
+            }
+
+            return View(amenityVM);
+        }
+
+        [HttpPost]
+        public IActionResult Update(AmenityVM amenityVM)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Amenity.Update(amenityVM.Amenity);
+                _unitOfWork.Save();
+                TempData["success"] = "Успешно обновлено";
+                return RedirectToAction(nameof(Index));
+            }
+
+            amenityVM.HotelList = _unitOfWork.Hotel.GetAll().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+
+            return View(amenityVM);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int amenityId)
+        {
+            AmenityVM amenityVM = new()
+            {
+                HotelList = _unitOfWork.Hotel.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+
+                Amenity = _unitOfWork.Amenity.Get(u => u.Id == amenityId)
+            };
+
+            if (amenityVM.Amenity is null)
+            {
+                RedirectToAction("Error", "Home");
+            }
+
+            return View(amenityVM);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(AmenityVM amenityVM)
+        {
+            Amenity? objFromDb = _unitOfWork.Amenity
+                .Get(u => u.Id == amenityVM.Amenity.Id);
+
+            if (objFromDb is not null)
+            {
+                _unitOfWork.Amenity.Remove(objFromDb);
+                _unitOfWork.Save();
+                TempData["success"] = "Успешно удалено";
+                return RedirectToAction("Index");
+            }
+
+            return View(amenityVM);
+        }
     }
 }
