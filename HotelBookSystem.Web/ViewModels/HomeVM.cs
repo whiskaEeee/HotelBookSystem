38dae0ab209185e1b778bb3f27f8feb53006a928
PC_home <grishakovdima456@gmail.com>
2025-05-05 22:20:32 +0300

    feat: add HomeVM class for hotel booking management

---
+++ HotelBookSystem.Web/ViewModels/HomeVM.cs
@@ -0,0 +1,13 @@
+﻿using HotelBookSystem.Domain.Entities;
+
+namespace HotelBookSystem.Web.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Hotel>? Hotels { get; set; }
+        public DateOnly CheckInDate { get; set; }
+        public DateOnly? CheckOutDate { get; set; }
+        public int NumberOfNights { get; set; }
+
+    }
+}
