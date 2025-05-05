31c91aeab0b8b6e5652c529002244de8bf7db4af
PC_home <grishakovdima456@gmail.com>
2025-05-05 07:39:36 +0300

    feat: add amenity interaface and realization

---
+++ HotelBookSystem.Application/Common/Interfaces/IAmenityRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using HotelBookSystem.Domain.Entities;
+
+namespace HotelBookSystem.Application.Common.Interfaces
+{
+    public interface IAmenityRepository : IRepository<Amenity>
+    {
+        void Update(Amenity entity);
+    }
+}
---
+++ HotelBookSystem.Domain/Entities/Amenity.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBookSystem.Domain.Entities
+{
+    public class Amenity
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        [ForeignKey("Hotel")]
+        public int HotelId { get; set; }
+        [ValidateNever]
+        public Hotel Hotel { get; set; }
+    }
+}
