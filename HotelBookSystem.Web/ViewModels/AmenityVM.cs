214195704c243e1b07bda903f7279cb9eb9ef841
Toshiro Kusahito <grishakovdima456@gmail.com>
2025-05-05 11:05:16 +0300

    feat: add AmenityVM class with HotelList property

---
+++ HotelBookSystem.Web/ViewModels/AmenityVM.cs
@@ -0,0 +1,13 @@
+﻿using HotelBookSystem.Domain.Entities;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HotelBookSystem.Web.ViewModels
+{
+    public class AmenityVM
+    {
+        public Amenity? Amenity { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem>? HotelList { get; set; }
+    }
+}
