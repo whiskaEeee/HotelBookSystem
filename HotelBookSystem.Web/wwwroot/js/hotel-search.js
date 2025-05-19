5e007f3695955dd6dda473d6f101801be501ab7a
Toshiro Kusahito <grishakovdima456@gmail.com>
2025-05-19 14:00:54 +0300

    feat: add fnLoadHotelList function for hotel search

---
+++ HotelBookSystem.Web/wwwroot/js/hotel-search.js
@@ -0,0 +1,17 @@
+﻿function fnLoadHotelList() {
+
+        var ObjData = {
+            NumberOfNights: $('#NumberOfNights').val(),
+            CheckInDate: $('#CheckInDate').val()
+        };
+
+        $.ajax({
+            type: "POST",
+            url: getHotelsUrl,
+            data: ObjData,
+            success: function (data) {
+                $('#hotelList').empty();
+                $('#hotelList').html(data);
+            }
+        });
+    }
