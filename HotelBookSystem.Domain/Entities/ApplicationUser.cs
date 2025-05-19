79eeff8d98ce0cabd8c93f4b1c5868e8834caa17
Toshiro Kusahito <grishakovdima456@gmail.com>
2025-05-19 10:53:08 +0300

    feat: add ApplicationUser class with properties

---
+++ HotelBookSystem.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace HotelBookSystem.Domain.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public DateOnly CreationDate { get; set; }
+
+    }
+}
