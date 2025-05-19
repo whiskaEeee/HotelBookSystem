0fdbaaa8cad444bb5414bf3f3587403de2933974
Toshiro Kusahito <grishakovdima456@gmail.com>
2025-05-19 10:53:34 +0300

    feat: add LoginVM and RegisterVM view models

---
+++ HotelBookSystem.Web/ViewModels/LoginVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookSystem.Web.ViewModels
+{
+    public class LoginVM
+    {
+        [Required]
+        public required string Email { get; set; }
+        [Required]
+        public required string Password { get; set; }
+        public bool RememberMe { get; set; }
+        public string? RedirectURL { get; set; }
+    }
+}
---
+++ HotelBookSystem.Web/ViewModels/RegisterVM.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HotelBookSystem.Web.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required]
+        public required string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name="Пароль")]
+        public required string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [Display(Name = "Подтвердите пароль")]
+        public required string ConfirmPassword { get; set; }
+        [Required]
+        [Display(Name="Имя")]
+        public required string Name { get; set; }
+        [Display(Name="Номер телефона")]
+        public string? PhoneNumber { get; set; }
+        public string? RedirectURL { get; set; }
+        public string? Role { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem>? RoleList { get; set; }
+    }
+}
