4050efc10223f1b18c384bc4fc0bb1e4889e5f02
Toshiro Kusahito <grishakovdima456@gmail.com>
2025-05-19 10:51:31 +0300

    feat: add StaticDetails class for role constants

---
+++ HotelBookSystem.Application/Utility/StaticDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBookSystem.Application.Utility
+{
+    public static class StaticDetails
+    {
+        public const string AdminRole = "Admin";
+        public const string CustomerRole = "Customer";
+    }
+}
