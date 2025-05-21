810cd5a75083d25d09f90aeb1dbf9ded5e0ff5b9
Toshiro Kusahito <grishakovdima456@gmail.com>
2025-05-21 14:57:43 +0300

    feat: add IBookingRepository and BookingRepository implementation

---
+++ HotelBookSystem.Application/Common/Interfaces/IBookingRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using HotelBookSystem.Domain.Entities;
+
+namespace HotelBookSystem.Application.Common.Interfaces
+{
+    public interface IBookingRepository : IRepository<Booking>
+    {
+        void Update(Booking entity);
+        void UpdateStatus(int bookingId, string bookingStatus);
+        void UpdateStripePaymentId(int bookingId, string sessionId, string paymentIntentId);
+    }
+}
---
+++ HotelBookSystem.Infrastructure/Repository/BookingRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using HotelBookSystem.Application.Common.Interfaces;
+using HotelBookSystem.Application.Utility;
+using HotelBookSystem.Domain.Entities;
+using HotelBookSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookSystem.Infrastructure.Repository
+{
+    public class BookingRepository : Repository<Booking>, IBookingRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookingRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Booking entity)
+        {
+            _db.Bookings.Update(entity);
+        }
+
+        public void UpdateStatus(int bookingId, string bookingStatus)
+        {
+            var bookingFromDb = _db.Bookings.FirstOrDefault(b => b.Id == bookingId);
+            if (bookingFromDb is not null)
+            {
+                bookingFromDb.Status = bookingStatus;
+
+                if (bookingStatus == StaticDetails.StatusCheckedIn)
+                {
+                    bookingFromDb.ActualCheckInDate = DateTime.Now;
+                }
+                if (bookingStatus == StaticDetails.StatusCompleted)
+                {
+                    bookingFromDb.ActualCheckOutDate = DateTime.Now;
+                }
+                _db.SaveChanges();
+            }
+        }
+
+        public void UpdateStripePaymentId(int bookingId, string sessionId, string paymentIntentId)
+        {
+            var bookingFromDb = _db.Bookings.FirstOrDefault(b => b.Id == bookingId);
+            if (bookingFromDb is not null)
+            {
+                if (!string.IsNullOrEmpty(sessionId))
+                {
+                    bookingFromDb.StripeSessionId = sessionId;
+                }
+                if (!string.IsNullOrEmpty(paymentIntentId))
+                {
+                    bookingFromDb.StripePaymentIntentId = paymentIntentId;
+                    bookingFromDb.IsPaymentSuccessful = true;
+                    bookingFromDb.PaymentDate = DateTime.Now;
+                }
+            }
+        }
+    }
+}
