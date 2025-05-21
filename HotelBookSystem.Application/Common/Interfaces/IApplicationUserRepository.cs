7c0fccea86cec92e7d36bd25500b9160291b6e44
Toshiro Kusahito <grishakovdima456@gmail.com>
2025-05-21 14:57:25 +0300

    feat: add IApplicationUserRepository and implementation

---
+++ HotelBookSystem.Application/Common/Interfaces/IApplicationUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using HotelBookSystem.Domain.Entities;
+
+namespace HotelBookSystem.Application.Common.Interfaces
+{
+    public interface IApplicationUserRepository : IRepository<ApplicationUser>
+    {
+    }
+}
---
+++ HotelBookSystem.Infrastructure/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using HotelBookSystem.Application.Common.Interfaces;
+using HotelBookSystem.Domain.Entities;
+using HotelBookSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookSystem.Infrastructure.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
